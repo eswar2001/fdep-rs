4c26833ca52c86a834c647dd73d417dcd818303f
eswar <eswar.tadiparth@juspay.in>
2025-07-21 16:58:51 +0530

    fetching LD_LIBRARY_PATH RUSTFLAGS

--- setup.sh
+++ setup.sh
@@ -1,7 +1,11 @@
-rustup install nightly-2023-08-01
-rustup component add --toolchain nightly-2023-08-01 rustc-dev
-rustup override set nightly-2023-08-01
-export RUSTFLAGS="-L ~/.rustup/toolchains/nightly-2023-08-01-aarch64-apple-darwin/lib"
-export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:~/.rustup/toolchains/nightly-2023-08-01-aarch64-apple-darwin/lib"
+rustup install nightly-2024-08-07
+rustup component add --toolchain nightly-2024-08-07 rustc-dev
+rustup override set nightly-2024-08-07
+SYSROOT=$(rustc --print sysroot)
+LIB_PATH="$SYSROOT/lib"
+export RUSTFLAGS="-L $LIB_PATH"
+export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$LIB_PATH"
+echo "RUSTFLAGS set to: $RUSTFLAGS"
+echo "LD_LIBRARY_PATH set to: $LD_LIBRARY_PATH"
 cargo install --debug --locked --path . --force
 cargo fdep
