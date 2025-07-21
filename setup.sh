6c136dd7ff8846ebd8191ce9c75070ccda308268
eswar <eswar.tadiparth@juspay.in>
2025-07-21 15:37:15 +0530

    fixed version diff issue

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
-rustup install nightly-2024-08-07
-rustup component add --toolchain nightly-2024-08-07 rustc-dev
-rustup override set nightly-2024-08-07
+rustup install nightly-2023-08-01
+rustup component add --toolchain nightly-2023-08-01 rustc-dev
+rustup override set nightly-2023-08-01
 export RUSTFLAGS="-L ~/.rustup/toolchains/nightly-2023-08-01-aarch64-apple-darwin/lib"
 export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:~/.rustup/toolchains/nightly-2023-08-01-aarch64-apple-darwin/lib"
 cargo install --debug --locked --path . --force
